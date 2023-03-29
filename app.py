f83abe5360cfa9a4fa24603bc83451a460fe34c4
adamjmcwilliams <adamjmcwilliams@gmail.com>
2023-03-29 14:48:27 -0700

    Basketball Database

--- app.py
+++ app.py
@@ -12,3 +12,20 @@ def db_test():
     conn = psycopg2.connect("postgres://adams_database_user:NK079LqRtrMghxVhVpKQtK2dFrzzYNiL@dpg-cgiaf9vdvk4vd526vf10-a/adams_database")
     conn.close()
     return 'Database connection successful'
+
+@app.route('/db_create')
+def db_create():
+    conn = psycopg2.connect("postgres://adams_database_user:NK079LqRtrMghxVhVpKQtK2dFrzzYNiL@dpg-cgiaf9vdvk4vd526vf10-a/adams_database")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''')
+    conn.commit()
+    conn.close()
+    return 'Basketball table created'
